3735a7c5be82802ad7a10c04875795bf0f88e189
SiddhantManocha <siddhantmanocha1994@gmail.com>
2016-01-29 02:07:52 +0530

    tunics

--- scraper_app/spiders/jabongURLs.py
+++ scraper_app/spiders/jabongURLs.py
@@ -1,7 +1,7 @@
 def getURL():
-	category="tops-tees-shirts"
+	category="tunics"
 	start_urls=list()
-	for i in range(1,342):
-	    tempUrl="http://www.jabong.com/women/clothing/tops-tees-shirts/?source=topnav_women&ax=1&page="+str(i)+"&limit=100&sortField=popularity&sortBy=desc"
+	for i in range(1,26):
+	    tempUrl="http://www.jabong.com/women/clothing/tunics/?source=topnav_women&ax=1&page="+str(i)+"&limit=100&sortField=popularity&sortBy=desc"
 	    start_urls.append(tempUrl)
 	return start_urls,category
